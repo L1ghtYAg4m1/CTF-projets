04b12ef0f3a931f47ad92cbb02734134126b193d
L1ghtYAg4m1 <johanaboubakarov@gmail.com>
2024-10-18 17:07:55 +0200

    Delete phenomenalshop directory

--- phenomenalshop/getwebhook.py
+++
@@ -1,4 +0,0 @@
-import requests
-TELEGRAM_BOT_TOKEN = '7321304401:AAFV6SmQBr0eNuEQMUoIrM07v78YNPgqmqU'
-response = requests.get(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getWebhookInfo")
-print(response.json())
--- phenomenalshop/setwebhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-TELEGRAM_BOT_TOKEN = '7321304401:AAFV6SmQBr0eNuEQMUoIrM07v78YNPgqmqU'
-NGROK_URL = 'https://d4a4-89-36-76-132.ngrok-free.app'  # Replace with your ngrok URL
-
-webhook_url = f"{NGROK_URL}/webhook"
-response = requests.post(f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook", data={'url': webhook_url})
-
-print(response.json())
